56195a5cfb87da6775b756326e099a0841e3c7f5
Mohosin <mohosinali075@gmail.com>
2025-11-25 15:38:27 +0600

    uncomment base url

--- src/lib/Base_URL.ts
+++ src/lib/Base_URL.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://buddiscript-server.onrender.com/api/v1";
-// export const MEDIA_URL = "https://buddiscript-server.onrender.com";
-export const BASE_URL = "http://192.168.0.106:5001/api/v1";
-export const MEDIA_URL = "http://192.168.0.106:5001";
+export const BASE_URL = "https://buddiscript-server.onrender.com/api/v1";
+export const MEDIA_URL = "https://buddiscript-server.onrender.com";
+// export const BASE_URL = "http://192.168.0.106:5001/api/v1";
+// export const MEDIA_URL = "http://192.168.0.106:5001";
