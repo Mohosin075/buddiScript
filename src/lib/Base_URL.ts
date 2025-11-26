ad689d65ad80b73f8cb1431407efc9170013c3a4
Md. Mohosin <mohosinali075@gmail.com>
2025-11-26 08:01:38 +0600

    update url

--- src/lib/Base_URL.ts
+++ src/lib/Base_URL.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://buddi-script-server.vercel.app/api/v1";
-export const MEDIA_URL = "https://buddi-script-server.vercel.app";
+export const BASE_URL = "https://www.mohosin.online/api/v1";
+// export const MEDIA_URL = "https://www.mohosin.online";
 // export const BASE_URL = "http://10.10.7.11:5001/api/v1";
 // export const MEDIA_URL = "http://10.10.7.11:5001";
